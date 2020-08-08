5aecb79a7056d1642337ec2a398e11aadcb2d06d
Michael MAtteis <michaelcmatteis@gmail.com>
2020-08-07 21:56:03 -0500

    making like buttin work and changing the size of the picture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import List from "./List"
+import List from "./List";
+
 
 
 
@@ -17,11 +18,10 @@ class App extends React.Component{
     fetch('https://api.punkapi.com/v2/beers')
         .then(res => res.json())
         .then(examples => {
-          console.log(examples)
             this.setState({beers: examples})
         })
   }
-  Counter (){
+  Counter = ()=>{
     this.setState({likes : this.state.likes + 1})
   }
 
@@ -31,7 +31,13 @@ class App extends React.Component{
         hjjhjhjhk
          <div >
            {this.state.beers.map((beer, index)=>{
-             return <List beers = {beer} key = {index} likes = {this.state.like} counter = {this.Counter}/>
+             return (
+               <div>
+                 <List beers = {beer} key = {index} likes = {this.state.likes} counter = {this.Counter} />
+                 <button onClick ={this.Counter}>like</button>
+                 <h2>{this.state.likes}</h2>  
+               </div>             
+              )
            })}
          </div>
       </div>
--- src/List.js
+++ src/List.js
@@ -1,17 +1,14 @@
-import React from "react"
-import App from "./App"
+import React from "react";
+import './Pics.css'
 
-// function Counter (props){
-//   this.setState({props.like})
-// }
+
 
 
 function List(props){
 
   return(
     <div >
-      <img src={props.beers.image_url}/>
-      <button onClick = {props.Counter}> Like </button> 
+      <img className = "size" src={props.beers.image_url}/>
       <h1>{props.beers.name}h1</h1>
       <h3>{props.beers.tagline}</h3>
       <p>{props.beers.description}</p>
